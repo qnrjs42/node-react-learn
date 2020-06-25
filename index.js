2e075c858edf40f2e4ebab010eb234bf1ecc5f42
BooGeon <qnrjs42@gmail.com>
2020-06-26 07:44:51 +0900

    Node JS // nodejs, express, 몽고 DB연결

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://master:<password>@reactlearn-apfyg.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected..'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example App listening on port ${port}`))
