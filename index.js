826bb0fc4c1e89f7375df0c8a0470aa766a63224
BooGeon <qnrjs42@gmail.com>
2020-06-30 03:51:14 +0900

    BodyParser, PostMan, 회원 가입 기능, 오타 수정

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 5000
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://master:<password>@reactlearn-apfyg.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://master:master123@reactlearn-apfyg.mongodb.net/reactLearn?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -12,5 +17,20 @@ mongoose.connect('mongodb+srv://master:<password>@reactlearn-apfyg.mongodb.net/<
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입시 필요한 정보들을 client에서 가져오면
+    // 그 정보들을 DB에 삽입
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example App listening on port ${port}`))
